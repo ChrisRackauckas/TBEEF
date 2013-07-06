a586ce0c1ede9b488570da121849df6dd813cfc0
Colin Jarvis <jarviscolin@hotmail.com>
2013-07-06 16:23:25 +0800

    bootstrap and appendColumns done

--- utils/utils.py
+++ utils/utils.py
@@ -87,4 +87,51 @@ def prependTxtToFile(inputPath,outputPath,txt):
     with file(inputPath, 'r') as original: \
            data = original.read()
     with file(outputPath, 'w') as modified: \
-           modified.write(txt + '\n' + data) 
+           modified.write(txt + '\n' + data)
+
+
+def bootstrap(inputPath, outputPath, nRows, random):
+# takes nRows-many random rows with replacement from infile and writes to outfile
+    fout = open(outputPath, 'w')
+    rows =[]
+    for line in open(inputPath, 'r'):
+        rows.append(line)
+    for i in range(nRows):
+        fout.write(rows[ random.randint(0, len(rows)-1) ])
+    fout.close()
+
+
+def appendColumns(infilePath1, infilePath2, outfilePath, outputSorted):
+# takes two data files and adds them together, grouping by user, sorting specified by outputSoreted
+    fin1 = open(infilePath1, 'r')
+    fin2 = open(infilePath2, 'r')
+    fout = open(outfilePath, 'w')
+    userSet = set()
+    userOrder = []      # order that users appear in fin1
+    linesByUser = {}    # all data lines by particular user
+    for line in fin1:
+        if line != '\n':
+            columns = line.split('\t')
+            user = int(columns[0])
+            if user not in userSet:
+                userSet.add(user)
+                userOrder.append(user)
+                linesByUser[user]=[]
+            linesByUser[user].append(line)
+    for line in fin2:
+        if line != '\n':
+            columns = line.split('\t')
+            user = int(columns[0])
+            if user not in userSet:
+                userSet.add(user)
+                userOrder.append(user)
+                linesByUser[user]=[]
+            linesByUser[user].append(line)
+    fin1.close()
+    fin2.close()
+    if outputSorted:
+        userOrder.sort()
+    for user in userOrder:
+            for line in linesByUser.get(user):
+                fout.write(line)
+    fout.close()
