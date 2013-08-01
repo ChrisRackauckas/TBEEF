2d0850485cebf724e1644161d7cde4f9935569c8
Ykid <choiwaikit.cwj@gmail.com>
2013-08-01 17:41:01 +0800

    revise write config function revise the setup function add the script.py back to the Svdmodel

--- utils/SVDModel.py
+++ utils/SVDModel.py
@@ -32,9 +32,10 @@ class SVDModel(Model):
     def setup(self):
         import utils
         import config
-        ### Boot to tmp ###
-        print("Re-Indexing")
-        values = self.reIndex()
+        if self.featureSet == 'Basic':
+            ### Boot to tmp ###
+            print("Re-Indexing")
+            values = self.reIndex()
         ### Take tmp to feat ###
         print("Setting Up Features")
         self.setupFeatures()
@@ -201,6 +202,8 @@ class SVDModel(Model):
             self.basicConvert(self.tmpTrain,self.featTrain)
             self.basicConvert(self.tmpCV,   self.featCV)
             self.basicConvert(self.tmpTest, self.featTest)
+        if self.featureSet == 'ImplicitFeedback':
+            self.setupImplicitFeatures()
 
     def basicConvert(self,fin,fout):
         fi = open( fin , 'r' )
@@ -238,3 +241,46 @@ class SVDModel(Model):
              ' name_pred=' + self.predTestTmp)
         self.prependUserMovieToPredictions(self.bootCV,self.predCVTmp,self.predCV)
         self.prependUserMovieToPredictions(self.bootTest,self.predTestTmp,self.predTest)       
+
+    def setupImplicitFeatures(self):
+    import os
+    #reindex the training files and build two dicts
+    Udic,ItemDic,avg=reIndex_Implicit(self.bootTrain,self.tmpTrain)
+    #reindex the history
+    transalte(self.userHistoryPath, self.userHistoryReindexPath, Udic, ItemDic)
+    #reindex CV file
+    translate(self.bootCV, self.tmpCV, Udic, ItemDic)
+    #reindex Testfile
+    translate(self.bootTest, self.tmpTest, Udic, ItemDic)
+
+    #make group training files
+    os.system(self.SVDFeatureLineReorder +' '+ self.tmpTrain + \
+            ' '+ self.tmpLineOrder)
+    os.system(self.SVDFeatureSVDPPRandOrder + ' ' + self.tmpTrain + \
+            ' ' + self.tmpLineOrder + ' ' + self.tmpGpTrain）
+
+    #make group training files of the CV set
+    os.system(self.SVDFeatureLineReorder +' '+ self.tmpCV + \
+            ' '+ self.tmpLineOrder)
+    os.system(self.SVDFeatureSVDPPRandOrder + ' ' + self.tmpCV + \
+            ' ' + self.tmpLineOrder + ' ' + self.tmpGpCV）
+
+    #make basic feature files
+    self.basicConvert(self.tmpGpTrain,self.featTrain)
+    self.basicConvert(self.tmpGpCV,   self.featCV)
+    self.basicConvert(self.tmpTest, self.featTest)
+
+    #make implicit feature files
+    mkImplicitFeatureFile(self.userHistoryReindexPath,self.tmpGpTrain,self.ImfeatTrain)
+    mkImplicitFeatureFile(self.userHistoryReindexPath,self.tmpTest,self.ImfeatTest)
+    mkImplicitFeatureFile(self.userHistoryReindexPath,self.tmpGpCV,self.ImfeatCV)
+
+    #set different parameters
+    self.numUser=len(Udic)
+    self.numMovie=len(ItemDic)
+    self.avg=avg
+    self.numGlobal = 0
+    self.activeType = 0
+    self.formatType = 1
+    self.numUserFeedback = len(ItemDic)
+    
